f39719b191d304c559ea09abbc18c05311fdb9a2
vishwa6785 <vishwa6785@gmail.com>
2017-01-05 23:26:34 +0530

    latest wsgi changes2

--- lwc/wsgi.py
+++ lwc/wsgi.py
@@ -9,24 +9,24 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lwc.settings")
-
-# application = get_wsgi_application()
-
-
-
-# try:
-# 	from dj_static import Cling
-# 	application = Cling(get_wsgi_application())
-
-# except:
-# 	pass
-
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lwc.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(lwc)
+
+
+
+try:
+	from dj_static import Cling
+	application = Cling(get_wsgi_application())
+
+except:
+	pass
+
+
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(lwc)
